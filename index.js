14d47403b92bd873e77bf59d284b84ed576a4e1c
Cosmodash <theo.github@bedafamily.com>
2020-11-18 13:19:23 -0800

    doppio.png babyyyyy

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 var discord = require('discord.js');
 var fs = require('fs');
 var client = new discord.Client();
+function sendimage(path, channel) {
+    var attachment = new discord.MessageAttachment();
+    attachment.setFile(path);
+    channel.send(attachment);
+}
 client.once('ready', function() {
     console.log('ready');
 });
 client.on('message', function(msg) {
     var message = msg.content;
-    var index = message.toLowerCase().indexOf('diavolo');
-    if ((msg.author.id != client.user.id) && (index != -1)) {
+    var valid_id = (msg.author.id != client.user.id);
+    var lower = message.toLowerCase();
+    var index = lower.indexOf('diavolo');
+    if (valid_id && (index != -1)) {
         msg.channel.send('diavolo');
+    }
+    index = lower.indexOf('doppio');
+    if (valid_id && (index != -1)) {
+        sendimage("./doppio.png", msg.channel);
     }
 });
 fs.readFile('token', 'utf8', function(err, data) {
